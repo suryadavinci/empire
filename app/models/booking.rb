43f674a36a5f05b7f9d63c39d78aaae3b0279cc1
Surya <suryadavinci@gmail.com>
2017-09-06 11:44:56 +0530

    Nested Attr under hold

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :journey
   belongs_to :user
   has_many :passengers, dependent: :destroy
+  accepts_nested_attributes_for :passengers,
+    :allow_destroy => true,
+    :reject_if     => :all_blank
+
 end
